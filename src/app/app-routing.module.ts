18f7ec0a5d41dd43e17103f16d029c81f0dd4999
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-08-14 10:53:53 -0300

    Navbar and bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
-
+  {
+    path: 'navbar',
+    component: NavbarComponent
+  }
 ];
 @NgModule({
   imports: [
