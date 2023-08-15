4d7dbb0061498fd605b3a2df1bce13cf2c9b9179
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-08-15 16:06:23 -0300

    api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,11 +1,21 @@
-//Um serviço dedicado para lidar com todas as chamadas API.
-
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private apiUrl = 'https://your-api-endpoint.com'; // Substitua pelo endpoint da sua API
+
+  constructor(private http: HttpClient) { }
+
+  // Método para coletar sinais do mercado aberto
+  getOpenMarketSignals(): Observable<any> {
+    const endpoint = `${this.apiUrl}/open-market-signals`; // Substitua pelo endpoint correto
+    return this.http.get(endpoint);
+  }
+
+  // Adicione outros métodos conforme necessário para outras chamadas API
 }
