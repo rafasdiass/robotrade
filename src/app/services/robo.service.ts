5ad78d932b219ab19a16bcfd2fdcc64b3d057f80
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-08-22 12:57:34 -0300

    tempo do robo

--- src/app/services/robo.service.ts
+++ src/app/services/robo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, timer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,32 +16,45 @@ export class RoboService {
     this.startPredictions();
   }
 
-  startPredictions() {
-    this.currencyPairs.forEach(pair => {
-      this.apiService.getStockData(pair).subscribe(data => {
-        const latestData = data['Time Series (5min)'];
-        for (const time in latestData) {
-          const closePrice = parseFloat(latestData[time]['4. close']);
-          
-          if (this.lastThreeCloses[pair].length >= 3) {
-            this.lastThreeCloses[pair].shift();
-          }
-          this.lastThreeCloses[pair].push(closePrice);
-
-          if (this.lastThreeCloses[pair].length === 3) {
-            const average = this.lastThreeCloses[pair].reduce((a, b) => a + b) / 3;
-            const lastClose = this.lastThreeCloses[pair][this.lastThreeCloses[pair].length - 1];
-            
-            if (average > lastClose) {
-              this.predictions$.next({ ...this.predictions$.getValue(), [pair]: 'Compra' });
-            } else {
-              this.predictions$.next({ ...this.predictions$.getValue(), [pair]: 'Venda' });
-            }
-          }
-        }
-      });
-    });
+  getNextFiveMinuteMark(): number {
+    const now = new Date();
+    const next = new Date(now);
+    next.setMinutes(Math.ceil(now.getMinutes() / 5) * 5);
+    next.setSeconds(0);
+    next.setMilliseconds(0);
+    return next.getTime() - now.getTime();
   }
+
+  startPredictions() {
+    const initialDelay = this.getNextFiveMinuteMark();
+    timer(initialDelay, 300000) // Starts at the next 5-min mark, then every 5 mins
+      .subscribe(() => {
+        this.currencyPairs.forEach(pair => {
+          this.apiService.getStockData(pair).subscribe((data: any) => {
+            const latestData = data['Time Series (5min)'];
+            for (const time in latestData) {
+              const closePrice = parseFloat(latestData[time]['4. close']);
+              if (this.lastThreeCloses[pair].length >= 3) {
+                this.lastThreeCloses[pair].shift();
+              }
+              this.lastThreeCloses[pair].push(closePrice);
+
+              if (this.lastThreeCloses[pair].length === 3) {
+                const average = this.lastThreeCloses[pair].reduce((a, b) => a + b) / 3;
+                const lastClose = this.lastThreeCloses[pair][this.lastThreeCloses[pair].length - 1];
+                
+                if (average > lastClose) {
+                  this.predictions$.next({ ...this.predictions$.getValue(), [pair]: 'Compra' });
+                } else {
+                  this.predictions$.next({ ...this.predictions$.getValue(), [pair]: 'Venda' });
+                }
+              }
+            }
+          });
+        });
+      });
+  }
+
   updateMovingAverage(pair: string, type: string, periods: number) {
     if (this.lastThreeCloses[pair].length >= periods) {
       let movingAverage = 0;
@@ -60,4 +73,3 @@ export class RoboService {
     return this.movingAverages[pair] || 0;
   }
 }
-
