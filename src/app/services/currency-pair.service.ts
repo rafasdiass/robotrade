655b681f55ba7085816e030f8f2d40cbcbd1f702
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-09-06 12:27:32 -0300

    moedas

--- src/app/services/currency-pair.service.ts
+++ src/app/services/currency-pair.service.ts
@@ -7,67 +7,45 @@ import { APIResponse, TimeSeries, TimeSeriesEntry } from '../models/api.interfac
   providedIn: 'root'
 })
 export class CurrencyPairService {
-  public currencyPairs$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
+
+  public currencyPairs$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(['EURUSD', 'AUDCAD', 'EURJPY', 'EURGBP']);
   public closingPrices5min$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
   public closingPrices15min$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
   public closingPrices1h$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
 
   constructor(private apiService: ApiService) {
-    this.updateCurrencyPairs();
+    console.log('Iniciando serviço de pares de moedas...');
   }
 
-  updateCurrencyPairs(): void {
-    this.apiService.getAllCurrencyPairs().subscribe(
-      (responses: APIResponse[]) => {
-        const allPairs: string[] = [];
-        responses.forEach(response => {
-          const keys = Object.keys(response);
-          keys.forEach(pair => {
-            if (/EUR|USD|JPY|AUD|CAD/.test(pair)) {
-              allPairs.push(pair);
-            }
-          });
-        });
-        this.currencyPairs$.next(allPairs);
-      },
-      error => {
-        console.log('Erro ao buscar dados da API:', error);
-      }
-    );
-  }
-
-  calculateFibonacciLevels(low: number, high: number): number[] {
+  public calculateFibonacciLevels(low: number, high: number): number[] {
     const fiboLevels = [0.382, 0.5, 0.618];
     return fiboLevels.map(level => low + (high - low) * level);
   }
 
-  fetchPriceData(ref: string, interval: string): void {
+  public fetchPriceData(ref: string, interval: string): void {
+    console.log(`Buscando dados de preço para ${ref} no intervalo ${interval}...`);
     this.apiService.getData(ref, interval).subscribe(
-      (response: APIResponse) => {
-        const key = `Time Series (${interval})`;
-        if (response && key in response) {  // Guarda de tipo
-          const timeSeries: TimeSeries = (response as any)[key];  // Type casting
-          const closingPrices: number[] = Object.values(timeSeries).map((entry: TimeSeriesEntry) => parseFloat(entry['4. close']));
-          if (interval === '5min') this.closingPrices5min$.next(closingPrices);
-          if (interval === '15min') this.closingPrices15min$.next(closingPrices);
-          if (interval === '1h') this.closingPrices1h$.next(closingPrices);
-        }
-      },
-      error => {
-        console.log(`Erro ao buscar dados de ${interval}:`, error);
-      }
+      (response: APIResponse) => this.processPriceDataResponse(response, interval),
+      error => console.log(`Erro ao buscar dados de ${interval}:`, error)
     );
   }
 
-  fetch5MinData(ref: string): void {
-    this.fetchPriceData(ref, '5min');
+  private processPriceDataResponse(response: APIResponse, interval: string): void {
+    const key = `Time Series (${interval})`;
+    if (response && key in response) {
+      const timeSeries: TimeSeries = (response as any)[key];
+      const closingPrices = this.extractClosingPrices(timeSeries);
+      this.updateClosingPrices(interval, closingPrices);
+    }
   }
 
-  fetch15MinData(ref: string): void {
-    this.fetchPriceData(ref, '15min');
+  private extractClosingPrices(timeSeries: TimeSeries): number[] {
+    return Object.values(timeSeries).map((entry: TimeSeriesEntry) => parseFloat(entry['4. close']));
   }
 
-  fetch1hData(ref: string): void {
-    this.fetchPriceData(ref, '1h');
+  private updateClosingPrices(interval: string, closingPrices: number[]): void {
+    if (interval === '5min') this.closingPrices5min$.next(closingPrices);
+    if (interval === '15min') this.closingPrices15min$.next(closingPrices);
+    if (interval === '1h') this.closingPrices1h$.next(closingPrices);
   }
 }
